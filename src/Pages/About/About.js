d56b2c7395afdd2ef47354066b7d265dcde845a3
SaraAzmin <azmin.sara17@gmail.com>
2022-04-17 23:50:11 +0200

    about added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>this is about</h2>
+        <div className='border-4 m-10 p-5'>
+            <h2 className='text-2xl font-bold mt-5'>Sara Azmin</h2>
+            <h4 className='text-sm md:text-lg'>MSc in Digital Transformation</h4>
+            <h4 className='text-sm md:text-lg'>Dortmund University of Applied Science & Arts</h4>
+            <p className='my-4 px-5 md:px-10 text-justify'>My key to archieve my goals is time management and patience. Patience helps me work well even in severe workloads. I always plan ahead to have a clear idea of what tasks I need to accomplish, how much time should be invested into it.  Also, instead of multi tasking, I prefer to fo one thing at a time. In that way my concentration is not devided into parts and I can think of multiple solution of one specific problem at a time. Working hard results better when one works smart as well. I have been using these tricks and will be using in future to achieve the goals I cherish.  </p>
+
         </div>
     );
 };
