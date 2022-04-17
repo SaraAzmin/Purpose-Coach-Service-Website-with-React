2f138fc6a2809715191877e3149d2199806be2d4
SaraAzmin <azmin.sara17@gmail.com>
2022-04-18 00:30:58 +0200

    readme file updated

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
-# Getting Started with Create React App
+# Purpose Coach Service Website
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## [Live Site Link](https://goodnotes-app-analysis.netlify.app/)
+
+Click here to see the live site: https://goodnotes-app-analysis.netlify.app/
+
+## Project Description
+
+This is a webiste for the services provided by a well known Purpose Coach, Author Jay Shetty. The website has following features:
+
+- Homepage has the services and costs of those services. Services can be choosen from there.
+
+- Homepage also has a section describing Genius coaching that is led solely by Jay Shetty himself. 
+
+- The website is well authenticated. Allows user to Resigter or Login in multiple ways such as email, google, github. 
+
+- User's checkout gateway is well protected. Only a registered user can see his/her bookings after loging in. 
+
+- The about page has a small tips and tricks section to manage work by the developer of this website.
+
+
+## Teck Stack Used
+
+- TailwindCSS
+- ReactJs
+- React Components
+- React Router
+- React Firebase Hooks
+- Firebase Authentication and Hosting
+
+
 
 ## Available Scripts
 
--- src/Pages/Home/GeniusCoaching/GeniusCoaching.js
+++ src/Pages/Home/GeniusCoaching/GeniusCoaching.js
@@ -11,27 +11,27 @@ const GeniusCoaching = () => {
                 <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-10 font-raleway">
                     <div className="shadow-lg shadow-blue-400 rounded-md md:mb-20 bg-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
                         <div className="p-5">
-                            <p className="my-3 text-gray-600">
+                            <p className="my-3 pb-5 text-gray-600">
                                 Genius is always in your own time and at your own pace. Listen to the workshops and meditations anytime, anywhere. Your growth is on your own terms.
                             </p>
-                            <a href="" className="font-bold text-blue-800 text-lg">Check Genius Blog</a>
+                            <a href="" className="font-bold text-blue-800 text-lg border-2 p-2 border-blue-800 rounded-lg">Check Genius Blog</a>
                         </div>
                     </div>
 
-                    <div className="shadow-lg shadow-blue-400 rounded-md bg-white md:mt-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                    <div className="shadow-lg shadow-blue-400 rounded-md bg-white md:mt-24 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
                         <div className="p-5">
-                            <p className="my-3 text-gray-600">
+                            <p className="my-3 text-gray-600 pb-5">
                                 Genius members get results. Within the first 90 days of Genius, members report feeling 25% less anxious, 40% less lonely, and 50% more focused.
                             </p>
-                            <a href="" className="font-bold text-blue-800 text-lg">Check Genius Blog</a>
+                            <a href="" className="font-bold text-blue-800 text-lg border-2 p-2 border-blue-800 rounded-lg">Check Genius Blog</a>
                         </div>
                     </div>
                     <div className="shadow-lg shadow-blue-400 rounded-md bg-white md:mb-20 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
                         <div className="p-5">
-                            <p className="my-3 text-gray-600">
+                            <p className="my-3 text-gray-600 pb-5">
                                 The Genius Community will be there for you for life. This is a global network dedicated to well-being, mindfulness, and peace. Our purpose is to serve.
                             </p>
-                            <a href="" className="font-bold text-blue-800 text-lg">Check Genius Blog</a>
+                            <a href="" className="font-bold text-blue-800 text-lg border-2 p-2 border-blue-800 rounded-lg">Check Genius Blog</a>
                         </div>
                     </div>
 
